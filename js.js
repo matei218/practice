916d352ae9e07639fd8933b3bd78f16aca25e115
mah-work <132890999+mah-work@users.noreply.github.com>
2024-10-10 11:56:20 +0200

    Ex1 - V1

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 /*
 JS - Write a JavaScript program to display the current day and time in the following format.  
 Sample Output : Today is : Tuesday.
-Current time is : 10 PM : 30 : 38
+Current time is : 10 : 30 : 38 PM
 */
 
 var date = new Date();
@@ -10,4 +10,33 @@ var today = date.getDay() - 1;
 
 var daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
+var hour = date.getHours();
+var minute = date.getMinutes();
+var second = date.getSeconds();
+
+var prepand = (hour >= 12) ? " PM " : " AM "
+
+hour = (hour >= 12) ? hour - 12 : hour;
+
+if(hour === 0 && prepand === 'PM'){
+    if(minute === 0 && second === 0){
+        hour = 12;
+        prepand = 'Noon'
+    }else{
+        hour = 12;
+        prepand = 'PM'
+    }
+}
+
+if(hour === 0 && prepand === 'AM'){
+    if(minute === 0 && second === 0){
+        hour = 12;
+        prepand = 'Midnight'
+    }else{
+        hour = 12;
+        prepand = 'AM'
+    }
+}
+
 console.log(`Today is ${daysOfWeek[today]}`);
+console.log(`Current time is: ${hour}:${minute}:${second} ${prepand}`);
