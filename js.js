4a6bad6e9f6285886e436f18fe5b8ce6b55924cf
mah-work <132890999+mah-work@users.noreply.github.com>
2024-10-10 10:42:09 +0200

    small changes

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
-console.log('hello world three');
+/*
+JS - Write a JavaScript program to display the current day and time in the following format.  
+Sample Output : Today is : Tuesday.
+Current time is : 10 PM : 30 : 38
+*/
+
+var date = new Date();
+
+var today = date.getDay() - 1;
+
+var daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+console.log(`Today is ${daysOfWeek[today]}`);
