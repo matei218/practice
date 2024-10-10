50b7ff6f3dda0d925c179036c562407018aec479
mah-work <132890999+mah-work@users.noreply.github.com>
2024-10-10 13:30:44 +0200

    Ex2 - Print page code added

---
+++ js_ex2.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to print the current window contents.  
+
+function printPage(){
+    window.print();
+}
+
+/*
+<div>
+    <button onclick="printPage()">Print current page</button>
+</div>
+*/
