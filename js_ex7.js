168cb1264978b2ca3947dc566df5656dffa267c9
mah-work <132890999+mah-work@users.noreply.github.com>
2024-10-11 14:34:37 +0200

    Ex7 - Check 1st day of the year

---
+++ js_ex7.js
@@ -0,0 +1,15 @@
+// Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.  
+
+/*
+lessons learned:
+get correct tiezone using Date.UTC() static method
+*/
+
+for(let y = 2014; y <= 2050; y++){
+    let d = new Date(Date.UTC(y, 0, 1));
+    if (d.getDay() === 0){
+        console.log(`Jan 1st, a Sunday in ${y}`);
+    }
+}
+
+// fiddle | https://jsfiddle.net/matei218/uremy867/
