9fd1c7b21cc8fb77f20679c0ca2dc192f9ad152c
matei218 <56275656+matei218@users.noreply.github.com>
2024-10-12 21:22:29 +0200

    Ex8 - Guess random number

--- js_ex8.js
+++ js_ex8.js
@@ -1,2 +1,21 @@
 /* Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. 
 The program displays a message "Good Work" if the input matches the guess number otherwise "Not matched". */  
+
+function guessNumber() {
+    let range = 10;
+    let randomNumber = Math.ceil(Math.random() * range);
+    let text;
+    
+    let inputValue = prompt("Enter a number between 0 and 9:");
+    
+    if (inputValue == null || inputValue === "") {
+        text = "No number entered. Please enter a number.";
+    } else {
+        let value = parseInt(inputValue, 10);
+        text = (randomNumber === value) ? `Good work. The random number is: ${randomNumber}` : `Not matched. The random number was: ${randomNumber}`;
+    }
+    
+    console.log(text); 
+}
+
+guessNumber();
