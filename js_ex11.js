99baa60501f09c700d7b469fcd3d75d5e9c688d3
matei218 <56275656+matei218@users.noreply.github.com>
2024-10-13 23:01:58 +0200

    Ex 11 - C to F and F to C

--- js_ex11.js
+++ js_ex11.js
@@ -1,1 +1,21 @@
-// hello world
+/*
+Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.  
+[ Formula : c/5 = (f-32)/9 [ where c = temperature in Celsius and f = temperature in Fahrenheit ]
+Expected Output :
+60°C is 140 °F
+45°F is 7.222222222222222°C
+*/
+
+const fahrenheitToCelcius = (f) => {
+    let celcius = ((f - 32) / 9) * 5;
+    console.log(`${f}°F is equal to ${celcius}°C;`);
+}
+
+fahrenheitToCelcius(45);
+
+const celciusToFahrenheit = (c) => {
+    let fahrenheit = ((c / 5) * 9) + 32;
+    console.log(`${c}°C is equal to ${fahrenheit}°F`);
+}
+
+celciusToFahrenheit(60);
