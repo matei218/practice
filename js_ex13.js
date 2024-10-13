76652231a8432fef48ae20c1249e8c9b940a3bfe
matei218 <56275656+matei218@users.noreply.github.com>
2024-10-13 23:20:46 +0200

    Ex13 - User-defined Variable

---
+++ js_ex13.js
@@ -0,0 +1,10 @@
+// Write a JavaScript exercise to create a variable using a user-defined name. 
+
+const varName = 'myVar';
+const v = 'var_value';
+
+this[varName] = v;
+
+console.log(this[varName]);
+
+// fiddle  | to come 
