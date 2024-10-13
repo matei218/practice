07fb5ec538222cfc83bf4644a1d2e49b9acd95dd
matei218 <56275656+matei218@users.noreply.github.com>
2024-10-13 23:14:56 +0200

    Ex12 - Get URL

---
+++ js_ex12.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program to get the website URL (loading page).  
+
+const webUrl = window.location.href;
+console.log(webUrl);
+
+// similar
+
+const webDocUrl = document.URL;
+console.log(webDocUrl);
