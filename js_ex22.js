1c4a39e2afb0cb9b271a6f737ce574475fbcbad2
matei218 <56275656+matei218@users.noreply.github.com>
2024-10-18 15:18:33 +0200

    added v2

--- js_ex22.js
+++ js_ex22.js
@@ -1,8 +1,19 @@
 // Write a JavaScript program to remove a character at the specified position in a given string and return the modified string.  
+
+// v1
 function removeCh(str, pos){
     return str.slice(0, pos) + str.slice(pos + 1);
 }
 
 console.log(removeCh('Matthew', 2));
+
+// v2
+
+function removeChTwo(str, pos){
+    let character = str.charAt(pos);
+    return str.replace(character,'');
+}
+
+console.log(removeChTwo('Benn', 3));
 
 // fiddle | to come
